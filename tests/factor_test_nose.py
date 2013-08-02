0e31dc91793872f84948eb972d303e0ce615b0d9
Francisco J Soto <fsoto@xinaptic.com>
2013-08-02 11:27:08 -0700

    setting up test setup

--- tests/factor_test_nose.py
+++ tests/factor_test_nose.py
@@ -1,155 +1,115 @@
 import Rvar
 import Factor
+from nose import with_setup
 
 epsilon = 0.00000000001
 # nosetests -v tests/factor_test_nose.py
 
-def test_number_3_4():
-    assert 2+2==4
-    
+def setup_func():
+    global v, f
+    v, f = {}, {}
+    v[1] = Rvar.Rvar(1, 2)
+    v[2] = Rvar.Rvar(2, 2)
+    v[3] = Rvar.Rvar(3, 2)
+    v[4] = Rvar.Rvar(4, 3)
+    f[1] = Factor.Factor([v[1]], [0.11, 0.89])
+    f[2] = Factor.Factor([v[2], v[1]], [0.59, 0.41, 0.22, 0.78])
+    f['w'] = Factor.Factor([v[3], v[2], v[1]], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0.15, 0.21])
+    f['z'] = Factor.Factor([v[3], v[2], v[4]], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
+    f['a'] = Factor.Factor([v[1]], [0.11, 0.89])
+    f['b'] = Factor.Factor([v[2], v[1]], [0.59, 0.41, 0.22, 0.78])
+    f['c'] = Factor.Factor([v[3], v[2]], [0.39, 0.61, 0.06, 0.94])
+    f['x'] = Factor.Factor([v[2], v[4]], [0.5, 0.8, 0.1, 0, 0.3, 0.9])
+    f['y'] = Factor.Factor([v[3], v[2]], [0.5, 0.7, 0.1, 0.2])
+    f['z'] = Factor.Factor([v[3], v[2], v[4]], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
+
+
 def test_cardinality_of_new_factor():
     a = Rvar.Rvar(1, 5)
     b = Rvar.Rvar(1, [0,1])
     x = Factor.Factor([a, b])
     assert x.cards==[5, 2]
 
+@with_setup(setup_func)
 def test_ass2idx_univariate_binomial():
-    v1 = Rvar.Rvar(1, 2)
-    f1 = Factor.Factor([v1], [0.11, 0.89])
-    assert f1.ass2idx([0]) == 0
-    assert f1.ass2idx([1]) == 1
+    assert f[1].ass2idx([0]) == 0
+    assert f[1].ass2idx([1]) == 1
 
+@with_setup(setup_func)
 def test_idx2ass_univariate_binomial():
-    v1 = Rvar.Rvar(1, 2)
-    f1 = Factor.Factor([v1], [0.11, 0.89])
-    assert f1.idx2ass([1]) == [[0]]
-    assert f1.idx2ass([2]) == [[1]]
+    assert f[1].idx2ass([1]) == [[0]]
+    assert f[1].idx2ass([2]) == [[1]]
 
+@with_setup(setup_func)
 def test_ass2idx_bivariate_binomial():
-    v1, v2 = Rvar.Rvar(1, 2), Rvar.Rvar(2, 2)
-    f2 = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    assert f2.ass2idx([0,0]) == 0
-    assert f2.ass2idx([1,0]) == 1
-    assert f2.ass2idx([0,1]) == 2
-    assert f2.ass2idx([1,1]) == 3
+    assert f[2].ass2idx([0,0]) == 0
+    assert f[2].ass2idx([1,0]) == 1
+    assert f[2].ass2idx([0,1]) == 2
+    assert f[2].ass2idx([1,1]) == 3
 
+@with_setup(setup_func)
 def test_ass2idx_bivariate_binomial():
-    v1, v2 = Rvar.Rvar(1, 2), Rvar.Rvar(2, 2)
-    f2 = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    assert f2.idx2ass([1,1]) == [[0,0],[0,0]]
-    assert f2.idx2ass([2]) == [[1,0]]
-    assert f2.idx2ass([3,4]) == [[0,1],[1,1]]
+    assert f[2].idx2ass([1,1]) == [[0,0],[0,0]]
+    assert f[2].idx2ass([2]) == [[1,0]]
+    assert f[2].idx2ass([3,4]) == [[0,1],[1,1]]
 
+@with_setup(setup_func)
 def test_ass2idx_multivariate_binomial():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(4, 3)
-    w = Factor.Factor([v3, v2, v1], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0.15, 0.21])
-    z = Factor.Factor([v3, v2, v4], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
-    assert w.ass2idx([1,0,1]) == 5
-    assert z.ass2idx([1,0,2]) == 9
-    assert z.ass2idx([1,1,0]) == 3
+    assert f['w'].ass2idx([1,0,1]) == 5
+    assert f['z'].ass2idx([1,0,2]) == 9
+    assert f['z'].ass2idx([1,1,0]) == 3
 
+@with_setup(setup_func)
 def test_idx2ass_multivariate_binomial():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(4, 3)
-    w = Factor.Factor([v3, v2, v1], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0.15, 0.21])
-    z = Factor.Factor([v3, v2, v4], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
-    assert w.idx2ass([6]) == [[1,0,1]]
-    assert z.idx2ass([10,4]) == [[1,0,2],[1,1,0]]
+    assert f['w'].idx2ass([6]) == [[1,0,1]]
+    assert f['z'].idx2ass([10,4]) == [[1,0,2],[1,1,0]]
 
+@with_setup(setup_func)
 def test_multiply_dimensions():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(4, 3)
-    
-    a = Factor.Factor([v1], [0.11, 0.89])
-    b = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])    
-    c = a.multiply_by(b)
-    print c.variables
-    assert c.variables == [v1, v2]
+    c = f['a'].multiply_by(f['b'])
+    assert c.variables == [v[1], v[2]]
     assert c.cards == [2, 2]
     
-    x = Factor.Factor([v2, v4], [0.5, 0.8, 0.1, 0, 0.3, 0.9])
-    y = Factor.Factor([v3, v2], [0.5, 0.7, 0.1, 0.2])
-    c = x.multiply_by(y)
-    assert c.variables == [v2, v3, v4]
+    c = f['x'].multiply_by(f['y'])
+    assert c.variables == [v[2], v[3], v[4]]
     assert c.cards == [2, 2, 3]
 
+@with_setup(setup_func)
 def test_multiply_values():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(4, 3)
-    
-    a = Factor.Factor([v1], [0.11, 0.89])
-    b = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    x = Factor.Factor([v2, v4], [0.5, 0.8, 0.1, 0, 0.3, 0.9])
-    y = Factor.Factor([v3, v2], [0.5, 0.7, 0.1, 0.2])
-    
-    c = a.multiply_by(b)
+    c = f['a'].multiply_by(f['b'])
     d = [0.0649, 0.1958, 0.0451, 0.6942]
     for e in [a - b for a, b in zip(c.values, d)]:
         assert e < epsilon
     
-    c = x.multiply_by(y)
+    c = f['x'].multiply_by(f['y'])
     print c.values
     d = [0.25, 0.08, 0.35, 0.16, 0.05, 0.0, 0.07, 0.0, 0.15, 0.09, 0.21, 0.18]
     for e in [a - b for a, b in zip(c.values, d)]:
         assert e < epsilon
 
-
+@with_setup(setup_func)
 def test_marginalize():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(1, 3)
-    
-    a = Factor.Factor([v1], [0.11, 0.89])
-    b = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    z = Factor.Factor([v3, v2, v4], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
-    
-    c = b.marginalize_by([v2])
+    c = f['b'].marginalize_by([v[2]])
     assert c.values == [0.5, 0.5]
     
-    c = z.marginalize_by([v2])
-    d = [0.20754716, 0.0314465, 0.15094339, 0.320754716, 0.0440251572, 0.245283018]
+    c = f['z'].marginalize_by([v[2]])
     print c.values
+    d = [0.20754716981132074, 0.320754716981132, 0.03144654088050314, 0.0440251572327044, \
+         0.15094339622641506, 0.2452830188679245]
     for e in [a - b for a, b in zip(c.values, d)]:
-        assert e < 0.0001
+        assert e < epsilon
 
+@with_setup(setup_func)
 def test_conditioning():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(1, 3)
-    evidence = {v2:0, v3:1}
-    
-    a = Factor.Factor([v1], [0.11, 0.89])
-    b = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    c = Factor.Factor([v3, v2], [0.39, 0.61, 0.06, 0.94])
-    z = Factor.Factor([v3, v2, v4], [0.25, 0.35, 0.08, 0.16, 0.05, 0.07, 0, 0, 0.15, 0.21, 0.09, 0.18])
-    
-    assert a.conditioned_by(evidence)==a
-    assert b.conditioned_by(evidence).values == [0.59, 0, 0.22, 0]
-    assert c.conditioned_by(evidence).values == [0, 0.61, 0, 0]
-    assert z.conditioned_by({v3:0}).values == [0.25, 0, 0.08, 0, 0.05, 0, 0, 0, 0.15, 0, 0.09, 0]
+    evidence = {v[2]:0, v[3]:1}
+    assert f['a'].conditioned_by(evidence)==f['a']
+    assert f['b'].conditioned_by(evidence).values == [0.59, 0, 0.22, 0]
+    assert f['c'].conditioned_by(evidence).values == [0, 0.61, 0, 0]
+    assert f['z'].conditioned_by({v[3]:0}).values == [0.25, 0, 0.08, 0, 0.05, 0, 0, 0, 0.15, 0, 0.09, 0]
 
+@with_setup(setup_func)
 def test_joint():
-    v1 = Rvar.Rvar(1, 2)
-    v2 = Rvar.Rvar(2, 2)
-    v3 = Rvar.Rvar(3, 2)
-    v4 = Rvar.Rvar(1, 3)
-
-    a = Factor.Factor([v1], [0.11, 0.89])
-    b = Factor.Factor([v2, v1], [0.59, 0.41, 0.22, 0.78])
-    c = Factor.Factor([v3, v2], [0.39, 0.61, 0.06, 0.94])
-
-    jf = Factor.Factor.joint([a, b, c])
+    jf = Factor.Factor.joint([f['a'], f['b'], f['c']])
     d = [0.025311, 0.076362, 0.002706, 0.041652, 0.039589, 0.119438, 0.042394, 0.652548]
     for e in [a - b for a, b in zip(jf.values, d)]:
         assert e < epsilon
@@ -165,13 +125,16 @@ def test_complete_1():
     v8 = Rvar.Rvar(8, 3)
     
     f1 = Factor.Factor([v1], [1.0/3.0, 1.0/3.0, 1.0/3.0])
-    f2 = Factor.Factor([v8, v2], [0.90000, 0.10000, 0.50000, 0.50000, 0.10000, 0.90000])
-    f3 = Factor.Factor([v3, v4, v7, v2], [0.90000, 0.10000, 0.80000, 0.20000, 0.70000, 0.30000, 0.60000, 0.40000, 0.40000, 0.60000, 0.30000, 0.70000, 0.20000, 0.80000, 0.10000, 0.90000])
+    f2 = Factor.Factor([v8, v2], [0.9, 0.1, 0.5, 0.5, 0.1, 0.9])
+    f3 = Factor.Factor([v3, v4, v7, v2], [0.9, 0.1, 0.8, 0.2, 0.7, \
+            0.3, 0.6, 0.4, 0.4, 0.6, 0.3, 0.7, 0.2, 0.8, 0.1, 0.9])
     f4 = Factor.Factor([v4], [0.5, 0.5])
-    f5 = Factor.Factor([v5, v6], [0.750000, 0.200000, 0.050000, 0.200000, 0.600000, 0.200000, 0.050000, 0.200000, 0.750000])
+    f5 = Factor.Factor([v5, v6], [0.75, 0.2, 0.05, 0.2, 0.6, 0.2, 0.05, 0.2, 0.75])
     f6 = Factor.Factor([v6], [0.3333, 0.3333, 0.3333])
-    f7 = Factor.Factor([v7, v5, v6], [0.90000, 0.10000, 0.80000, 0.20000, 0.70000, 0.30000, 0.60000, 0.40000, 0.50000, 0.50000, 0.40000, 0.60000, 0.30000, 0.70000, 0.20000, 0.80000, 0.10000, 0.90000])
-    f8 = Factor.Factor([v8, v4, v1], [0.100000, 0.300000, 0.600000, 0.050000, 0.200000, 0.750000, 0.200000, 0.500000, 0.300000, 0.100000, 0.350000, 0.550000, 0.800000, 0.150000, 0.050000, 0.200000, 0.600000, 0.200000])
+    f7 = Factor.Factor([v7, v5, v6], [0.9, 0.1, 0.8, 0.2, 0.7, \
+            0.3, 0.6, 0.4, 0.5, 0.5, 0.4, 0.6, 0.3, 0.7, 0.2, 0.8, 0.1, 0.9])
+    f8 = Factor.Factor([v8, v4, v1], [0.1, 0.3, 0.6, 0.05, 0.2,\
+            0.75, 0.2, 0.5, 0.3, 0.1, 0.35, 0.55, 0.8, 0.15, 0.05, 0.2, 0.6, 0.2])
     
     jf = Factor.Factor.joint([f1, f2, f3, f4, f5, f6, f7, f8]).marginalize_by([v2,v3,v4,v5,v6,v7,v8])
     d = [0.3741, 0.3027, 0.3231]
