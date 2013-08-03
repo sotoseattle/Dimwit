dca512657e5de84753b46556ad4ace91188f1c9b
Francisco J Soto <fsoto@xinaptic.com>
2013-08-02 19:08:10 -0700

    basic logistic regression finishing touches

--- Logysterical.py
+++ Logysterical.py
@@ -1,47 +1,74 @@
 import math
-#import pandas as pd
+import pandas as pd
 import numpy as np
 from scipy import optimize
 
 class Logysterical(object):
-    '''All related to Ligistic Regression'''
+    '''Logistic Regression with regularization and BFGS optimization'''
     
-    def __init__(self, X, y):
-        self.m = X.shape[0]
-        self.X = np.column_stack([np.ones((self.m,1)), X]) # already has the ones!
-        self.y = y
+    def __init__(self, training_X, training_y, lam=0.0):
+        self.lam = lam
+        self.m, n = training_X.shape
+        self.Xt = np.column_stack([np.ones((self.m,1)), training_X])
+        self.yt = training_y
+        self.thetas = np.zeros((n+1, 1))
     
     def costFunction(self, theta):
         '''Computes cost and gradient for logistic regression'''
-        prediction = Logysterical.sigmoid(np.sum(self.X*theta,axis=1))
-        J = (1.0/self.m)*(-self.y.T.dot(np.log(prediction)) - \
-            (1-self.y).T.dot(np.log(1-prediction)))
+        prediction = Logysterical.sigmoid(np.dot(self.Xt, theta))
+        J = (1.0/self.m)*(-self.yt.T.dot(np.log(prediction)) - \
+            (1-self.yt).T.dot(np.log(1-prediction))) +    \
+            (self.lam / (2.0*self.m)) * np.sum(np.power(theta[1::], 2))
         return J
+    
+    def gradientFunction(self, theta):
+        prediction = Logysterical.sigmoid(np.dot(self.Xt, theta))
+        regulam = theta # REFACTOR
+        regulam[0] = 0
+        grad = (1.0/self.m)*((prediction - self.yt).dot(self.Xt)) + regulam*(self.lam/self.m)
+        return grad
+    
+    def optimizeThetas(self, theta0):
+        self.thetas = optimize.fmin_bfgs(f=self.costFunction, x0=theta0, \
+              fprime=self.gradientFunction, maxiter=40000).flatten()
+        return self.thetas
+    
+    def predict(self, x):
+        return Logysterical.sigmoid(x.dot(self.thetas))
+    
+    def accuracy(self, x, y):
+        acc = 0.0
+        for i in range(self.m):
+            p = self.predict(x[i,::])
+            if (p>0.5 and y[i]==1) or (p<=0.5 and y[i]==0):
+                    acc +=1
+        return acc/self.m
+        
     
     @classmethod
     def sigmoid(cls, z):
         '''computes dot sigmoid of a np.array as vector or matrix'''
-        return np.vectorize(lambda x: 1/(1+math.exp(-x)))(z)
+        return np.vectorize(lambda x: 1/(1+np.exp(-x) + 1E-11))(z)
     
 
+    
 
-
-
-
-
-##a = Logysterical()
-##print Logysterical.sigmoid(0)
-##print Logysterical.sigmoid([0, 1, 0.75, 4])
-##print Logysterical.sigmoid([[0, 1, 0.75, 4],[-40, 40], 0])
-
-#data = pd.read_csv('./tests/data_example.txt')
-#l = Logysterical(data.ix[:, 0:2], data.ix[:, 2])
-#theta = np.zeros_like(l.X)
-#print l.costFunction(theta)
-
-
+#data = pd.read_csv('./tests/log_data_basic.txt')
+##data = pd.read_csv('./tests/log_data_reg.txt')
+#X = np.array(data.ix[:, 0:2])
+#y = np.array(data.ix[:, 2])
+#l = Logysterical(X, y, 0)
+#ini_thetas = np.array([0,0,0])
+#print ini_thetas
+#print l.optimizeThetas(ini_thetas)
+#print "-"*40
+#print l.thetas
 #
-##y = np.array([2,2,2])
-##x = np.array([3,2,1])
-##theta = np.array([1,2,3])
-##print Logysterical.costFunction(theta, x, y)
+#t0 = np.array([0,0,0])
+#prob = l.predict(np.array([1.0, 45, 85]))
+#print prob
+#
+#p = l.accuracy(l.Xt, l.yt)
+#print p
+
+
--- tests/logysterical_test_nose.py
+++ tests/logysterical_test_nose.py
@@ -3,17 +3,45 @@ import numpy as np
 import Logysterical
 from nose import with_setup
 
-epsilon = 0.00000001
+epsilon = 0.000001
 
 def setup_func():
     global l
-    data = pd.read_csv('./tests/data_example.txt')
+    data = pd.read_csv('./tests/log_data_basic.txt')
     X = data.ix[:, 0:2]
     y = data.ix[:, 2]
-    l = Logysterical.Logysterical(X, y)
+    l = Logysterical.Logysterical(X, y, 0.0)
 
 
 @with_setup(setup_func)
-def test_J_with_no_theta():
-    theta = np.zeros_like(l.X)
+def test_J_with_zero_theta():
+    theta = np.array([0,0,0])
     assert l.costFunction(theta) - 0.69314718056 < epsilon
+
+@with_setup(setup_func)    
+def test_gradient_with_zero_theta():
+    theta = np.array([0,0,0])
+    diff = l.gradientFunction(theta) - np.array([-0.1, -12.009217, -11.262842])
+    assert diff.max() < epsilon
+
+@with_setup(setup_func)
+def test_optimize_thetas():
+    ini_thetas = np.array([0,0,0])
+    opt_thetas = l.optimizeThetas(ini_thetas)
+    diff = opt_thetas - np.array([-25.1613364, 0.20623176, 0.2014716])
+    assert diff.max() < epsilon
+
+@with_setup(setup_func)
+def test_prediction():
+    ini_thetas = np.array([0,0,0])
+    opt_thetas = l.optimizeThetas(ini_thetas)
+    prob = l.predict(np.array([1, 45, 85]))
+    assert prob - 0.776290570502 < epsilon
+
+@with_setup(setup_func)    
+def test_accuracy():
+    ini_thetas = np.array([0,0,0])
+    opt_thetas = l.optimizeThetas(ini_thetas)
+    assert l.accuracy(l.Xt, l.yt) - 0.89 < epsilon
+    
+
