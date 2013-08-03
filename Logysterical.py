200d27291ce1e65a2afe00afd05c96201cf517b7
Francisco J Soto <fsoto@xinaptic.com>
2013-08-03 13:31:17 -0700

    log regr regularization works

--- Logysterical.py
+++ Logysterical.py
@@ -1,36 +1,43 @@
 import math
 import pandas as pd
 import numpy as np
-from scipy import optimize
+import scipy as sc
+from scipy.optimize import fmin_bfgs
+
+#### Use decorator pattern to integrate cost and gradient in a single method
+#### refactor further grad - regulam in a single line
+#### avoid gradient calc in test or fix method
+#### print l.predict(mapfeaturenp.array([1, 45, 85]))
 
 class Logysterical(object):
     '''Logistic Regression with regularization and BFGS optimization'''
     
     def __init__(self, training_X, training_y, lam=0.0):
         self.lam = lam
-        self.m, n = training_X.shape
-        self.Xt = np.column_stack([np.ones((self.m,1)), training_X])
+        self.training_samples, n = training_X.shape
+        self.training_features = n+1
+        self.Xt = np.column_stack([np.ones((self.training_samples,1)), training_X])
         self.yt = training_y
-        self.thetas = np.zeros((n+1, 1))
+        self.thetas = np.zeros((self.training_features, 1))
     
     def costFunction(self, theta):
         '''Computes cost and gradient for logistic regression'''
         prediction = Logysterical.sigmoid(np.dot(self.Xt, theta))
-        J = (1.0/self.m)*(-self.yt.T.dot(np.log(prediction)) - \
+        J = (1.0/self.training_samples)*(-self.yt.T.dot(np.log(prediction)) - \
             (1-self.yt).T.dot(np.log(1-prediction))) +    \
-            (self.lam / (2.0*self.m)) * np.sum(np.power(theta[1::], 2))
+            (self.lam / (2.0*self.training_samples)) * np.sum(np.power(theta[1::], 2))
         return J
     
     def gradientFunction(self, theta):
         prediction = Logysterical.sigmoid(np.dot(self.Xt, theta))
-        regulam = theta # REFACTOR
+        regulam = theta.T*(self.lam/self.training_samples)
         regulam[0] = 0
-        grad = (1.0/self.m)*((prediction - self.yt).dot(self.Xt)) + regulam*(self.lam/self.m)
+        grad = (1.0/self.training_samples)*((prediction - self.yt).dot(self.Xt)) + regulam
         return grad
     
     def optimizeThetas(self, theta0):
-        self.thetas = optimize.fmin_bfgs(f=self.costFunction, x0=theta0, \
-              fprime=self.gradientFunction, maxiter=40000).flatten()
+        self.thetas = fmin_bfgs(f=self.costFunction, x0=theta0, \
+              fprime=self.gradientFunction, maxiter=4000).flatten()
         return self.thetas
     
     def predict(self, x):
@@ -38,11 +45,11 @@ class Logysterical(object):
     
     def accuracy(self, x, y):
         acc = 0.0
-        for i in range(self.m):
+        for i in range(self.training_samples):
             p = self.predict(x[i,::])
             if (p>0.5 and y[i]==1) or (p<=0.5 and y[i]==0):
                     acc +=1
-        return acc/self.m
+        return acc/self.training_samples
         
     
     @classmethod
@@ -50,13 +57,26 @@ class Logysterical(object):
         '''computes dot sigmoid of a np.array as vector or matrix'''
         return np.vectorize(lambda x: 1/(1+np.exp(-x) + 1E-11))(z)
     
-
+    @classmethod
+    def mapfeature(cls, X1, X2):
+        '''maps two input features to quadratic polinomiasl. 
+        X1,X2 => X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..'''
+        Xt = np.zeros((X1.shape[0],1))
+        degree = 6
+        for i in range(1,degree+1):
+            for j in range(0,i+1):
+                Xt = np.column_stack([Xt, np.power(X1, i-j) * np.power(X2, j)])
+        return Xt[:,1::] # first column not needed
     
 
+
+
+
+
 #data = pd.read_csv('./tests/log_data_basic.txt')
-##data = pd.read_csv('./tests/log_data_reg.txt')
-#X = np.array(data.ix[:, 0:2])
-#y = np.array(data.ix[:, 2])
+data = pd.read_csv('./tests/log_data_reg.txt')
+X = np.array(data.ix[:, 0:2])
+y = np.array(data.ix[:, 2])
 #l = Logysterical(X, y, 0)
 #ini_thetas = np.array([0,0,0])
 #print ini_thetas
@@ -70,5 +90,25 @@ class Logysterical(object):
 #
 #p = l.accuracy(l.Xt, l.yt)
 #print p
+
+
+X1 = np.array(X[:,0])
+X2 = np.array(X[:,1])
+Xext = Logysterical.mapfeature(X1, X2)
+
+l = Logysterical(Xext, y, 1.0)
+ini_thetas = np.zeros((l.training_features, 1))
+#print l.gradientFunction(ini_thetas)
+
+print l.optimizeThetas(ini_thetas)
+print l.accuracy(l.Xt, l.yt)
+
+
+
+
+
+
+
+
 
 
--- tests/logysterical_test_nose.py
+++ tests/logysterical_test_nose.py
@@ -1,47 +1,90 @@
 import pandas as pd
 import numpy as np
-import Logysterical
+from Logysterical import * 
 from nose import with_setup
 
 epsilon = 0.000001
 
-def setup_func():
+def setup_func_basic():
     global l
     data = pd.read_csv('./tests/log_data_basic.txt')
     X = data.ix[:, 0:2]
     y = data.ix[:, 2]
-    l = Logysterical.Logysterical(X, y, 0.0)
+    l = Logysterical(X, y, 0.0)
 
+def setup_func_reg():
+    global train
+    train = {}
+    data = pd.read_csv('./tests/log_data_reg.txt')
+    x = np.array(data.ix[:, 0:2])
+    X1 = x[:,0]
+    X2 = x[:,1]
+    train['X'] = Logysterical.mapfeature(X1, X2)
+    train['y'] = np.array(data.ix[:, 2])
+    
 
-@with_setup(setup_func)
+@with_setup(setup_func_basic)
 def test_J_with_zero_theta():
     theta = np.array([0,0,0])
     assert l.costFunction(theta) - 0.69314718056 < epsilon
 
-@with_setup(setup_func)    
+@with_setup(setup_func_basic)    
 def test_gradient_with_zero_theta():
     theta = np.array([0,0,0])
     diff = l.gradientFunction(theta) - np.array([-0.1, -12.009217, -11.262842])
     assert diff.max() < epsilon
 
-@with_setup(setup_func)
+@with_setup(setup_func_basic)
 def test_optimize_thetas():
     ini_thetas = np.array([0,0,0])
     opt_thetas = l.optimizeThetas(ini_thetas)
     diff = opt_thetas - np.array([-25.1613364, 0.20623176, 0.2014716])
     assert diff.max() < epsilon
 
-@with_setup(setup_func)
+@with_setup(setup_func_basic)
 def test_prediction():
     ini_thetas = np.array([0,0,0])
     opt_thetas = l.optimizeThetas(ini_thetas)
     prob = l.predict(np.array([1, 45, 85]))
     assert prob - 0.776290570502 < epsilon
 
-@with_setup(setup_func)    
+@with_setup(setup_func_basic)    
 def test_accuracy():
     ini_thetas = np.array([0,0,0])
     opt_thetas = l.optimizeThetas(ini_thetas)
     assert l.accuracy(l.Xt, l.yt) - 0.89 < epsilon
+
+
+### WITH REGULARIZATION
+
+
+@with_setup(setup_func_reg)
+def test_J_with_lam_and_zero_theta():
+    l = Logysterical(train['X'], train['y'], 1.0)
+    theta = np.zeros((l.Xt.shape[1],1))
+    assert l.costFunction(theta) - 0.69314718056 < epsilon
+
+def test_mapping_features():
+    data = pd.read_csv('./tests/log_data_reg.txt')
+    x = np.array(data.ix[:, 0:2])
+    X1 = x[:,0]
+    X2 = x[:,1]
+    qq = Logysterical.mapfeature(X1, X2)
+    m,n = qq.shape
+    for i in range(m):
+        assert qq[i,0] - X1[i] == 0
+        assert qq[i,1] - X2[i] == 0
+
+@with_setup(setup_func_reg)
+def test_optimize_thetas_reg():
+    l = Logysterical(train['X'], train['y'], 1.0)
+    ini_thetas = np.zeros((l.Xt.shape[1],1))
+    diff = l.optimizeThetas(ini_thetas) - np.array([1.273005, 0.624876, 1.177376, -2.020142, -0.912616, -1.429907, 0.125668, -0.368551, -0.360033, -0.171068, -1.460894, -0.052499, -0.618889, -0.273745, -1.192301, -0.240993, -0.207934, -0.047224, -0.278327, -0.296602, -0.453957, -1.045511, 0.026463, -0.294330, 0.014381, -0.328703, -0.143796, -0.924883])
+    for e in diff:
+        assert e < epsilon
+
+
+
     
+
 
